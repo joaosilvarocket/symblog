1afbb786f70e0267357fc2064085d66e6397d09d
Joao Silva <joao.silva@jovago.com>
2015-01-29 01:00:58 +0000

    symblog all proj

---
+++ symblog/app/cache/dev/twig/6a/a2/39df83a3e1a7c3b50f6c077d37142a19cf34506c8cba532d1149ed0ff38b.php
@@ -0,0 +1,37 @@
+<?php
+
+/* WebProfilerBundle:Profiler:ajax_layout.html.twig */
+class __TwigTemplate_6aa239df83a3e1a7c3b50f6c077d37142a19cf34506c8cba532d1149ed0ff38b extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+            'panel' => array($this, 'block_panel'),
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        $this->displayBlock('panel', $context, $blocks);
+    }
+
+    public function block_panel($context, array $blocks = array())
+    {
+        echo "";
+    }
+
+    public function getTemplateName()
+    {
+        return "WebProfilerBundle:Profiler:ajax_layout.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  20 => 1,);
+    }
+}

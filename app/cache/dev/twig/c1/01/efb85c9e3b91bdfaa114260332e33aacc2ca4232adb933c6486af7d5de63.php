723b09d61f75213432cfb1d9290b93288a2f5cdf
Joao Silva <joao.silva@jovago.com>
2015-01-30 00:00:30 +0000

    clear cache

---
+++ app/cache/dev/twig/c1/01/efb85c9e3b91bdfaa114260332e33aacc2ca4232adb933c6486af7d5de63.php
@@ -0,0 +1,57 @@
+<?php
+
+/* BloggerBlogBundle:Page:index.html.twig */
+class __TwigTemplate_c101efb85c9e3b91bdfaa114260332e33aacc2ca4232adb933c6486af7d5de63 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        try {
+            $this->parent = $this->env->loadTemplate("BloggerBlogBundle::layout.html.twig");
+        } catch (Twig_Error_Loader $e) {
+            $e->setTemplateFile($this->getTemplateName());
+            $e->setTemplateLine(1);
+
+            throw $e;
+        }
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "BloggerBlogBundle::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "    Blog homepage
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "BloggerBlogBundle:Page:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  39 => 4,  36 => 3,  11 => 1,);
+    }
+}
